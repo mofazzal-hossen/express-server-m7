9d7ab40631e218ea4f2ea55e68106e516381922d
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-05-22 00:23:12 +0600

    feat: add PostgreSQL integration with pg package and update server routes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,31 @@
-import express from 'express'
+import express from 'express';
+import type { Request, Response } from 'express';
+import { Pool } from "pg"
+
 const app = express()
 const port = 9000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.use(express.json());
+
+const pool = new Pool({
+    connectionString: "postgresql://neondb_owner:npg_1HZDcMxe0AVY@ep-dark-glade-apu5y3t7-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+
 })
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "now i am learning in express js",
+        status: "successfully"
+    })
+});
+
+app.post('/products', async (req: Request, res: Response) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'product received',
+        data: req.body
+    });
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
