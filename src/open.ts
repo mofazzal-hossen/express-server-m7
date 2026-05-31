73f342344e00b725c051b89d7c42e0585853081f
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-05-31 22:21:23 +0600

    error

--- src/open.ts
+++ src/open.ts
@@ -1,3 +1,29 @@
+// class BankAccount {
+//   // Private field: cannot be accessed directly outside this class
+//   #balance;
+
+//   constructor(initialBalance) {
+//     this.#balance = initialBalance;
+//   }
+
+//   // Public method to deposit money safely
+//   deposit(amount) {
+//     if (amount > 0) this.#balance += amount;
+//   }
+
+//   // Getter method to view the private property safely
+//   get balance() {
+//     return `Your balance is $${this.#balance}`;
+//   }
+// }
+
+// const account = new BankAccount(100);
+// account.deposit(50);
+// console.log(account.balance); // Output: Your balance is $150
+// // console.log(account.#balance); // Error: Private field '#balance' must be declared in an enclosing class
+
+
+
 class BankAccount {
   // Private field: cannot be accessed directly outside this class
   #balance;
@@ -17,7 +43,4 @@ class BankAccount {
   }
 }
 
-const account = new BankAccount(100);
-account.deposit(50);
-console.log(account.balance); // Output: Your balance is $150
-// console.log(account.#balance); // Error: Private field '#balance' must be declared in an enclosing class
+
